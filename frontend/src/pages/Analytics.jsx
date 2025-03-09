2533cf979328ed2a7baa9e6d273706b30ef36c29
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2025-03-09 14:37:14 +0530

    come to v1

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "../components/analytics/Search";
+
 export default function Analytics(){
     return <div className="searchComponentX">
         <Search />
