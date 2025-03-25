07d302c98f8ce68a4f559be9a164bebb75cbf7c1
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2025-03-25 16:17:42 +0530

    remove contentComponent

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ContentComponent from "../components/content/index.jsx";
 import AssignmentViewer from "../components/content/assignment-viewer/index.jsx";
 
 export default function Content() {
